9a752161a03aa85aabffd1939fca9c5032d7edae
Vesku18 <vesa.simila@yahoo.com>
2021-09-27 18:17:40 +0300

    1.5

--- osa1/src/App.js
+++ osa1/src/App.js
@@ -40,28 +40,31 @@ const Total = (props) =>{
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    { 
-    name:'Fundamentals of React',
-    exercises: 10
-    },
-    {
-    name:'Using props to pass data',
-    exercises: 7
-    },
-    {
-    name:'State of a component',
-    exercises: 14
-    }
-  ]
+  const course = {
+    name:'Half Stack application development',
+    parts: [
+      { 
+      name:'Fundamentals of React',
+      exercises: 10
+      },
+      {
+      name:'Using props to pass data',
+      exercises: 7
+      },
+      {
+      name:'State of a component',
+      exercises: 14
+      }
+    ]
+  }
   const totalLabel = 'Number of exercises'  
+
 
   return (
     <div>
-      <Header otsikko = {course}/>
-      <Content p = {parts} />
-      <Total total = {parts[0].exercises + parts[1].exercises + parts[2].exercises} totalLabel = {totalLabel} />
+      <Header otsikko = {course.name}/>
+      <Content p = {course.parts} />
+      <Total total = {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} totalLabel = {totalLabel} />
     </div>
   )
 }
